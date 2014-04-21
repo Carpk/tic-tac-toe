7ebce3e36923bb979c9a03b0990bc7a36e2076f7
vinco <zappeln@gmail.com>
2014-04-20 22:29:52 -0500

    adding rails12 gem to fix heroku deplyment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Sample::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false # this may cause the doubling
+  config.assets.compile = true # this may cause the doubling
 
   # Generate digests for assets URLs.
   config.assets.digest = true

8cfcd7ac3e418a1caab45b2ad02bd9b072a4bc18
Shawn Klein <zappeln@gmail.com>
2015-04-11 10:31:06 -0500

    refactor minimax for increased speed
    4

--- app/models/computerai.rb
+++ app/models/computerai.rb
@@ -3,12 +3,13 @@ class ComputerAi
   def initialize(params)
     @game_piece = params[:symbol]
     @enemy_piece = params[:opponent]
+    @efficent_value = -1
   end
 
   def assert_values(board)
     position_values = {}
     board.indexes_of_available_spaces.each do |empty_position|
-      possible_board = Marshal.load( Marshal.dump(board) )
+      possible_board = TicTacToeBoard.new(board.grid.clone)
       possible_board.assign_token_to(@game_piece, empty_position)
       position_values[empty_position] = evaluate_board(possible_board, @enemy_piece, @game_piece)
     end
@@ -17,12 +18,17 @@ class ComputerAi
   end
 
   def evaluate_board(board, current_player, passing_player, depth=1)
-    return create_value(board) / depth if gameover?(board) || depth > 6
+    return create_value(board) / depth if gameover?(board) || @efficent_value > (1.0 / depth )
+
     board_values = []
+
     board.indexes_of_available_spaces.each do |empty_position|
-      played_board = Marshal.load( Marshal.dump(board) )
+      played_board = TicTacToeBoard.new(board.grid.clone)
       played_board.assign_token_to(current_player, empty_position)
+
       board_values << evaluate_board(played_board, passing_player, current_player, depth +1)
+
+      @efficent_value = board_values.last if board_values.last > @efficent_value
     end
 
     if current_player == @game_piece

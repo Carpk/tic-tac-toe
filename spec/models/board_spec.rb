e42a3f934f5f279aa6a47bfea4282bfebce6887a
brechen <zappeln@gmail.com>
2016-04-22 15:58:23 -0500

    change board array to use variables in GamePlay rspec tests

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 describe Board do
 
-  let(:board)  {Board.new(Array.new(9," "))}
+  let(:board)  {Board.new(Array.new(9,n))}
+  let(:o) {Appdata::TOKENS[:computer]}  
+  let(:x) {Appdata::TOKENS[:player]}
+  let(:n) {Appdata::TOKENS[:blank]}
 
   it "should return the correct length of board" do
     expect(board.grid.length).to eq(9)
   end
 
   it "should return a blank array" do
-    blank_array = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    blank_array = [n, n, n, n, n, n, n, n, n]
     expect(Board.new_board).to eq(blank_array)
   end
 
@@ -28,17 +31,17 @@ describe Board do
   end
 
   it "should return false for not having unassigned positions on board" do
-    grid = ["o", "x", "o",
-            "x", "o", "x",
-            "x", "o", "x"]
+    grid = [o, x, o,
+            x, o, x,
+            x, o, x]
     board = Board.new(grid)
     expect(board.unassigned_positions?).to eq(false)
   end
 
   it "should return true for having a unassigned position on board" do
-    grid = ["o", "x", "o",
-            "x", " ", "x",
-            "x", "o", "x"]
+    grid = [o, x, o,
+            x, n, x,
+            x, o, x]
     board = Board.new(grid)
     expect(board.unassigned_positions?).to eq(true)
   end
@@ -48,31 +51,31 @@ describe Board do
   end
 
   it "should return false for empty position on board" do
-    grid = ["o", "x", "o",
-            "x", " ", "x",
-            "x", "o", "x"]
+    grid = [o, x, o,
+            x, n, x,
+            x, o, x]
     board = Board.new(grid)
     expect(board.board_full?).to eq(false)
   end
 
   it "should return false for empty board" do
-    grid = ["o", "x", "o",
-            "x", "o", "x",
-            "x", "o", "x"]
+    grid = [o, x, o,
+            x, o, x,
+            x, o, x]
     board = Board.new(grid)
     expect(board.board_full?).to eq(true)
   end
 
   it "should assign player to board position" do
-    token, position = "r", 7
+    token, position = o, 7
     board.assign_token_to(token, position)
-    expect(board.grid[7]).to eq("r")
+    expect(board.grid[7]).to eq(o)
   end
 
   it "should assign player to board position" do
-    token, position = "x", 3
+    token, position = x, 3
     board.assign_token_to(token, position)
-    expect(board.grid[3]).to eq("x")
+    expect(board.grid[3]).to eq(x)
   end
 
   it "should return the square root for board of 9" do
@@ -81,42 +84,42 @@ describe Board do
   end
 
   it "should return the square root for board of 12" do
-    grid = ["o", "x", "o", " ",
-            "x", "o", "x", " ",
-            "x", "o", "x", " ",
-            " ", " ", " ", " "]
+    grid = [o, x, o, n,
+            x, o, x, n,
+            x, o, x, n,
+            n, n, n, n]
     board = Board.new(grid)
     expect(board.board_side_length).to eq(4)
   end
 
   it "should return the index positions of availible board positions" do
-    grid = ['f','f','f',
-            "g"," "," ",
-            " ","g"," "]
+    grid = [o,o,o,
+            x,n,n,
+            n,x,n]
     board = Board.new(grid)
     expect(board.indexes_of_available_spaces).to eq([4,5,6,8])
   end
 
   it "should return index positions of availible board positions" do
-    grid = ["f"," ","f",
-            "g"," ","f",
-            " ","g","f"]
+    grid = [o,n,o,
+            x,n,o,
+            n,x,o]
     board = Board.new(grid)
     expect(board.indexes_of_available_spaces).to eq([1,4,6])
   end
 
   it "should return empty array with full board" do
-    grid = ["f","g","f",
-            "g","g","f",
-            "f","g","f"]
+    grid = [o,x,o,
+            x,x,o,
+            o,x,o]
     board = Board.new(grid)
     expect(board.indexes_of_available_spaces).to eq([])
   end
 
   it "should return all positions with empty board" do
-    grid = [" "," "," ",
-            " "," "," ",
-            " "," "," "]
+    grid = [n,n,n,
+            n,n,n,
+            n,n,n]
     board = Board.new(grid)
     expect(board.indexes_of_available_spaces).to eq([0,1,2,3,4,5,6,7,8])
   end
--- spec/models/gameplay_spec.rb
+++ spec/models/gameplay_spec.rb
@@ -2,264 +2,267 @@ require 'rails_helper'
 
 describe GamePlay do
 
-  let(:game)      {GamePlay.new(Board.new(Array.new(9," ")))}
-
+  let(:game)      {GamePlay.new(Board.new(Array.new(9,n)))}
+  let(:o) {Appdata::TOKENS[:computer]}  
+  let(:x) {Appdata::TOKENS[:player]}
+  let(:n) {Appdata::TOKENS[:blank]}
+  
   it "should know that a game is over from a full board" do
-    grid = ["x", "o", "x",
-            "o", "x", "o",
-            "o", "x", "o"]
+    grid = [x, o, x,
+            o, x, o,
+            o, x, o]
     game = GamePlay.new(Board.new(grid))
     expect(game.gameover?).to eq(true)
   end
 
   it "should know that a game is over from a winning board" do
-    grid = ["x", "o", " ",
-            "o", "x", "o",
-            "o", "x", "x"]
+    grid = [x, o, n,
+            o, x, o,
+            o, x, x]
     game = GamePlay.new(Board.new(grid))
     expect(game.gameover?).to eq(true)
   end
 
   it "should know that a game is not over" do
-    grid = ["x", "o", " ",
-            "o", "x", "o",
-            "o", "x", " "]
+    grid = [x, o, n,
+            o, x, o,
+            o, x, n]
     game = GamePlay.new(Board.new(grid))
     expect(game.gameover?).to eq(false)
   end
 
   it "should know that a game is not over with empty board" do
-    grid = [" ", " ", " ",
-            " ", " ", " ",
-            " ", " ", " "]
+    grid = [n, n, n,
+            n, n, n,
+            n, n, n]
     game = GamePlay.new(Board.new(grid))
     expect(game.gameover?).to eq(false)
   end
 
   it "should return false if there is no winner" do
-    grid = ["x", "o", " ",
-            "o", "x", "o",
-            "o", "x", " "]
+    grid = [x, o, n,
+            o, x, o,
+            o, x, n]
     game = GamePlay.new(Board.new(grid))
     expect(game.board_has_winner?).to eq(false)
   end
 
   it "should return true if there is a matching array" do
-    grid = ["x", "o", " ",
-            "o", "x", "o",
-            "x", "x", "x"]
+    grid = [x, o, n,
+            o, x, o,
+            x, x, x]
     game = GamePlay.new(Board.new(grid))
     expect(game.board_has_winner?).to eq(true)
   end
 
   it "should return true if there is a matching array" do
-    grid = ["o", "o", "o",
-            "o", "x", "o",
-            "x", "o", "x"]
+    grid = [o, o, o,
+            o, x, o,
+            x, o, x]
     game = GamePlay.new(Board.new(grid))
     expect(game.board_has_winner?).to eq(true)
   end
 
     it "should return true if there is a matching array" do
-    grid = ["x", "x", " ",
-            "o", "o", "o",
-            "x", "o", "x"]
+    grid = [x, x, n,
+            o, o, o,
+            x, o, x]
     game = GamePlay.new(Board.new(grid))
     expect(game.board_has_winner?).to eq(true)
   end
 
   it "should return false if only matches are empty cells" do
-    grid = ["x", "o", " ",
-            "o", "x", " ",
-            "x", "x", " "]
+    grid = [x, o, n,
+            o, x, n,
+            x, x, n]
     game = GamePlay.new(Board.new(grid))
     expect(game.board_has_winner?).to eq(false)
   end
 
   it "should return false if only matches are empty cells" do
-    grid = [" ", " ", " ",
-            " ", " ", " ",
-            " ", " ", " "]
+    grid = [n, n, n,
+            n, n, n,
+            n, n, n]
     game = GamePlay.new(Board.new(grid))
     expect(game.board_has_winner?).to eq(false)
   end
 
   it "should return false if only matches are empty cells" do
-    grid = ["x", "o", " ",
-            "x", "x", " ",
-            "x", "o", " "]
+    grid = [x, o, n,
+            x, x, n,
+            x, o, n]
     game = GamePlay.new(Board.new(grid))
     expect(game.board_has_winner?).to eq(true)
   end
 
   it "should return false if only matches are empty cells" do
-    grid = ["x", "o", " ",
-            "o", "o", " ",
-            "x", "o", " "]
+    grid = [x, o, n,
+            o, o, n,
+            x, o, n]
     game = GamePlay.new(Board.new(grid))
     expect(game.board_has_winner?).to eq(true)
   end
 
   it "should return false if only matches are empty cells" do
-    grid = ["x", "o", "o",
-            "o", "x", "o",
-            "x", "o", "o"]
+    grid = [x, o, o,
+            o, x, o,
+            x, o, o]
     game = GamePlay.new(Board.new(grid))
     expect(game.board_has_winner?).to eq(true)
   end
 
   it "should return false if only matches are empty cells" do
-    grid = ["x", "o", " ",
-            "o", "x", " ",
-            "x", "o", "x"]
+    grid = [x, o, n,
+            o, x, n,
+            x, o, x]
     game = GamePlay.new(Board.new(grid))
     expect(game.board_has_winner?).to eq(true)
   end
 
   it "should return false if only matches are empty cells" do
-    grid = ["o", "o", "x",
-            "o", "x", " ",
-            "x", "x", "o"]
+    grid = [o, o, x,
+            o, x, n,
+            x, x, o]
     game = GamePlay.new(Board.new(grid))
     expect(game.board_has_winner?).to eq(true)
   end
 
   it "should return correct winner of board" do
-    grid = ["x", "o", " ",
-            "x", "x", " ",
-            "x", "o", " "]
+    grid = [x, o, n,
+            x, x, n,
+            x, o, n]
     game = GamePlay.new(Board.new(grid))
-    expect(game.symbol_of_winner).to eq("x")
+    expect(game.symbol_of_winner).to eq(x)
   end
 
   it "should return correct winner of board" do
-    grid = ["x", "o", " ",
-            "o", "o", " ",
-            "x", "o", " "]
+    grid = [x, o, n,
+            o, o, n,
+            x, o, n]
     game = GamePlay.new(Board.new(grid))
-    expect(game.symbol_of_winner).to eq("o")
+    expect(game.symbol_of_winner).to eq(o)
   end
 
   it "should return correct winner of board" do
-    grid = ["x", "o", "o",
-            "o", "x", "o",
-            "x", "o", "o"]
+    grid = [x, o, o,
+            o, x, o,
+            x, o, o]
     game = GamePlay.new(Board.new(grid))
-    expect(game.symbol_of_winner).to eq("o")
+    expect(game.symbol_of_winner).to eq(o)
   end
 
   it "should return winner of a winning board" do
-    grid = ["x", "o", " ",
-            "o", " ", "o",
-            "x", "x", "x"]
+    grid = [x, o, n,
+            o, n, o,
+            x, x, x]
     game = GamePlay.new(Board.new(grid))
-    expect(game.symbol_of_winner).to eq("x")
+    expect(game.symbol_of_winner).to eq(x)
   end
 
   it "should return winner of a winning board" do
-    grid = ["x", "o", " ",
-            "o", "o", "o",
-            "x", " ", "x"]
+    grid = [x, o, n,
+            o, o, o,
+            x, n, x]
     game = GamePlay.new(Board.new(grid))
-    expect(game.symbol_of_winner).to eq("o")
+    expect(game.symbol_of_winner).to eq(o)
   end
 
   it "should return true if there is a matching array" do
-    grid = ["o", "o", "o",
-            "o", "x", "o",
-            "x", "o", "x"]
+    grid = [o, o, o,
+            o, x, o,
+            x, o, x]
     game = GamePlay.new(Board.new(grid))
-    expect(game.symbol_of_winner).to eq("o")
+    expect(game.symbol_of_winner).to eq(o)
   end
 
     it "should return true if there is a matching array" do
-    grid = ["x", "x", " ",
-            "o", "o", "o",
-            "x", "o", "x"]
+    grid = [x, x, n,
+            o, o, o,
+            x, o, x]
     game = GamePlay.new(Board.new(grid))
-    expect(game.symbol_of_winner).to eq("o")
+    expect(game.symbol_of_winner).to eq(o)
   end
 
   it "should return true if there is a matching array" do
-    grid = ["x", "o", " ",
-            "o", "x", "o",
-            "x", "x", "x"]
+    grid = [x, o, n,
+            o, x, o,
+            x, x, x]
     game = GamePlay.new(Board.new(grid))
-    expect(game.symbol_of_winner).to eq("x")
+    expect(game.symbol_of_winner).to eq(x)
   end
 
   it "should return false if only matches are empty cells" do
-    grid = ["x", "o", " ",
-            "o", "x", " ",
-            "x", "o", "x"]
+    grid = [x, o, n,
+            o, x, n,
+            x, o, x]
     game = GamePlay.new(Board.new(grid))
-    expect(game.symbol_of_winner).to eq("x")
+    expect(game.symbol_of_winner).to eq(x)
   end
 
   it "should return false if only matches are empty cells" do
-    grid = ["o", "o", "x",
-            "o", "x", " ",
-            "x", "x", "o"]
+    grid = [o, o, x,
+            o, x, n,
+            x, x, o]
     game = GamePlay.new(Board.new(grid))
-    expect(game.symbol_of_winner).to eq("x")
+    expect(game.symbol_of_winner).to eq(x)
   end
 
   it "should return true if board is tied" do
-    grid = ["x", "o", "x",
-            "x", "o", "x",
-            "o", "x", "o"]
+    grid = [x, o, x,
+            x, o, x,
+            o, x, o]
     game = GamePlay.new(Board.new(grid))
     expect(game.tie_game?).to eq(true)
   end
 
   it "should return false if board is not full" do
-    grid = ["x", "o", " ",
-            "x", "o", "x",
-            "o", "x", "o"]
+    grid = [x, o, n,
+            x, o, x,
+            o, x, o]
     game = GamePlay.new(Board.new(grid))
     expect(game.tie_game?).to eq(false)
   end
 
   it "should return false if board has a winner" do
-    grid = ["o", "o", " ",
-            "x", "o", "x",
-            "o", "x", "o"]
+    grid = [o, o, n,
+            x, o, x,
+            o, x, o]
     game = GamePlay.new(Board.new(grid))
     expect(game.tie_game?).to eq(false)
   end
 
   it "should be false if first cell is empty" do
-    section = [" ", "g", "g"]
+    section = [n, o, o]
     expect(game.group_match?(section)).to eq(false)
   end
 
   it "should be false if second cell is empty" do
-    section = ["g", " ", "g"]
+    section = [o, n, o]
     expect(game.group_match?(section)).to eq(false)
   end
 
   it "should be false if last cell is empty" do
-    section = ["g", "g", " "]
+    section = [o, o, n]
     expect(game.group_match?(section)).to eq(false)
   end
 
   it "should be false if argument contains only 2 the same character" do
-    section = ["g", "x", "g"]
+    section = [o, x, o]
     expect(game.group_match?(section)).to eq(false)
   end
 
   it "should be false if argument contains only 2 the same character" do
-    section = ["g", "g", "x"]
+    section = [o, o, x]
     expect(game.group_match?(section)).to eq(false)
   end
 
   it "should be false if section is completely empty" do
-    section = [" ", " ", " "]
+    section = [n, n, n]
     expect(game.group_match?(section)).to eq(false)
   end
 
   it "should be true if argument contains the same character" do
-    section = ["g", "g", "g"]
+    section = [o, o, o]
     expect(game.group_match?(section)).to eq(true)
   end
 
@@ -268,25 +271,25 @@ describe GamePlay do
   end
 
   it "should know if game is over with player win" do
-    board = [" ", "o", " ",
-             " ", "o", " ",
-             " ", "o", " "]
+    board = [n, o, n,
+             n, o, n,
+             n, o, n]
     game = GamePlay.new(Board.new(board))
     expect(game.gameover?).to eq(true)
   end
 
   it "should know if game is over with a full board" do
-    board = ["x", "o", "x",
-             "o", "x", "o",
-             "o", "x", "o"]
+    board = [x, o, x,
+             o, x, o,
+             o, x, o]
     game = GamePlay.new(Board.new(board))
     expect(game.gameover?).to eq(true)
   end
 
   it "should know if game is not over with 1 empty cell" do
-    board = [" ", "o", "x",
-             "o", "x", "o",
-             "o", "x", "o"]
+    board = [n, o, x,
+             o, x, o,
+             o, x, o]
     game = GamePlay.new(Board.new(board))
     expect(game.gameover?).to eq(false)
   end
@@ -296,17 +299,17 @@ describe GamePlay do
   end
 
   it "should correctly set player position" do
-    board = [" ", " ", " ",
-             " ", "o", " ",
-             " ", " ", " "]
+    board = [n, n, n,
+             n, o, n,
+             n, n, n]
     game = GamePlay.new(Board.new(board))
-    expect(game.display_board[4]).to eq("o")
+    expect(game.display_board[4]).to eq(o)
   end
 
   it "should get a number response from the ai" do
-    board = ["x", " ", "x",
-             " ", "o", " ",
-             " ", " ", "o"]
+    board = [x, n, x,
+             n, o, n,
+             n, n, o]
     game = GamePlay.new(Board.new(board))
     expect(game.computer_turn.class).to eq(String)
   end

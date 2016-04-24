a025a8c949399cba6ceff97a546e5c74ea76e3dc
brechen <zappeln@gmail.com>
2016-04-24 10:40:20 -0500

    remove unnecessary tests, used #send for deeply nested methods

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -20,7 +20,7 @@ describe Board do
     expect(board.valid?).to eq(true)
   end
 
-  it "should not validate an improper array" do
+  it "should not validate an array with a long string" do
     grid = [n, n, n,
             n, "Hi", n,
             n, n, n]
@@ -28,7 +28,7 @@ describe Board do
     expect(board.valid?).to eq(false)
   end
 
-  it "should not validate an improper array" do
+  it "should not validate an array with a boolean" do
     grid = [n, n, n,
             n, n, n,
             true, n, n]
@@ -36,7 +36,7 @@ describe Board do
     expect(board.valid?).to eq(false)
   end
 
-  it "should not validate an improper array" do
+  it "should not validate a non-array object" do
     grid = Object.new
     board = Board.new(grid)
     expect(board.valid?).to eq(false)
@@ -52,24 +52,20 @@ describe Board do
     expect(board.grid.length).to eq(4)
   end
 
-  xit "should return true for having multiple unassigned positions on board" do
-    expect(board.unassigned_positions?).to eq(true)
-  end
-
-  xit "should return false for not having unassigned positions on board" do
+  it "should return false for not having unassigned positions on board" do
     grid = [o, x, o,
             x, o, x,
             x, o, x]
     board = Board.new(grid)
-    expect(board.unassigned_positions?).to eq(false)
+    expect(board.send(:unassigned_positions?)).to eq(false)
   end
 
-  xit "should return true for having a unassigned position on board" do
+  it "should return true for having a unassigned position on board" do
     grid = [o, x, o,
             x, n, x,
             x, o, x]
     board = Board.new(grid)
-    expect(board.unassigned_positions?).to eq(true)
+    expect(board.send(:unassigned_positions?)).to eq(true)
   end
 
   it "should return false for empty board" do
@@ -84,7 +80,7 @@ describe Board do
     expect(board.board_full?).to eq(false)
   end
 
-  it "should return false for empty board" do
+  it "should return true for filled board" do
     grid = [o, x, o,
             x, o, x,
             x, o, x]
@@ -104,18 +100,17 @@ describe Board do
     expect(board.grid[3]).to eq(x)
   end
 
-  xit "should return the square root for board of 9" do
-
-    expect(board.board_side_length).to eq(3)
+  it "should return the square root for board of 9" do
+    expect(board.send(:board_side_length)).to eq(3)
   end
 
-  xit "should return the square root for board of 12" do
+  it "should return the square root for board of 12" do
     grid = [o, x, o, n,
             x, o, x, n,
             x, o, x, n,
             n, n, n, n]
     board = Board.new(grid)
-    expect(board.board_side_length).to eq(4)
+    expect(board.send(:board_side_length)).to eq(4)
   end
 
   it "should return the index positions of availible board positions" do
@@ -150,23 +145,15 @@ describe Board do
     expect(board.indexes_of_available_spaces).to eq([0,1,2,3,4,5,6,7,8])
   end
 
-  it "should return nested arry of all possible wins" do
+  it "should return nested array of all possible wins" do
     expect(board.possible_wins.length).to eq(8)
   end
 
-  xit "should return nested array for rows" do
-    expect(board.row_sections[2].class).to eq(Array)
+  it "should return 3 cell count for win combination" do
+    expect(board.possible_wins.first.length).to eq(3)
   end
 
-  xit "should return nested array for columns" do
-    expect(board.column_sections[2].class).to eq(Array)
-  end
-
-  xit "should return nested array for forwardslash " do
-    expect(board.forwardslash_section.class).to eq(Array)
-  end
-
-  xit "should return nested array for backslash " do
-    expect(board.backslash_section.class).to eq(Array)
+  it "should return 3 cell count for win combination" do
+    expect(board.possible_wins.last.length).to eq(3)
   end
 end
--- spec/models/computer_ai_spec.rb
+++ spec/models/computer_ai_spec.rb
@@ -47,95 +47,32 @@ describe ComputerAi do
     expect(computer.next_position_for(board)).to eq(4)
   end
 
-  xit "should evaluate the board and return an number" do
+  it "should evaluate the board and return an number" do
     grid = [b, b, b,
             b, x, o,
             b, b, b]
     board = Board.new(grid)
-    expect(computer.position_value(board, x, o)).to eq(0)
+    expect(computer.send(:position_value, board, x, o)).to eq(0)
   end
 
-  xit "should choose the hash with the highest value" do
-    hash = { 3 => -0.3, 6 => 0.1, 7 => -0.3}
-    expect(computer.select_random_index(hash)).to eq(6)
+  it "should choose the hash with the highest value" do
+    hash = { 3 => -0.3, 6 => 0.3, 7 => 0.1}
+    expect(computer.send(:select_random_index, hash)).to eq(6)
   end
 
-  xit "should choose the hash with the highest value" do
-    hash = { 3 => -0.3, 6 => 0.1, 7 => -0.3, 8 => 1.0}
-    expect(computer.select_random_index(hash)).to eq(8)
-  end
-
-  xit "should choose the hash with the highest value" do
-    hash = { 3 => -0.3, 4 => -0.1, 7 => -0.3}
-    expect(computer.select_random_index(hash)).to eq(4)
-  end
-
-  xit "should choose the hash with the highest value" do
-    hash = { 3 => -0.3, 6 => 0.1, 7 => 0.1}
-    expect([6,7].include?(computer.select_random_index(hash))).to eq(true)
-  end
-
-  xit "should return true if game is over with full board" do
-    grid = [o, x, o,
-            x, o, x,
-            x, o, x]
-    board = Board.new(grid)
-    expect(computer.gameover?(board)).to eq(true)
-  end
-
-  xit "should return true if game is over with diagonal win" do
-    grid = [o, x, o,
-            x, o, x,
-            b, b, o]
-    board = Board.new(grid)
-    expect(computer.gameover?(board)).to eq(true)
-  end
-
-  xit "should return false if game is not over" do
-    grid = [x, x, o,
-            x, o, x,
-            b, b, o]
-    board = Board.new(grid)
-    expect(computer.gameover?(board)).to eq(false)
-  end
-
-  xit "should return a 0 value based on tie game" do
-    grid = [o, x, o,
-            x, o, x,
-            x, o, x]
-    board = Board.new(grid)
-    expect(computer.value_of(board)).to eq(0)
-  end
-
-  xit "should create a negitve value based on board lose" do
+  it "should create a negitve value based on board lose" do
     grid = [o, o, o,
             x, b, x,
             b, b, b]
     board = Board.new(grid)
-    expect(computer.value_of(board)).to eq(-362880)
+    expect(computer.send(:value_of,board)).to be < 0
   end
 
-  xit "should create a positive value based on incomplete board win" do
+  it "should create a positive value based on incomplete board win" do
     grid = [x, b, b,
             o, x, o,
             b, b, x]
     board = Board.new(grid)
-    expect(computer.value_of(board)).to eq(362880)
-  end
-
-  xit "returns correct value of last position winning move" do
-    grid = [o, x, o,
-            x, x, x,
-            o, o, x]
-    board = Board.new(grid)
-    expect(computer.value_of(board)).to eq(362880)
-  end
-
-  xit "returns zero value when board is incomplete" do
-    grid = [o, x, o,
-            b, x, x,
-            o, o, x]
-    board = Board.new(grid)
-    expect(computer.value_of(board)).to eq(0)
+    expect(computer.send(:value_of, board)).to be > 0
   end
 end
